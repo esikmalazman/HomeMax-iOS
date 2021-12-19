dd7009d6e94554e67d9c6ea036f040019104e8b3
Ikmal Azman <opikmalazman@gmail.com>
2021-12-20 00:16:22 +0800

    init helper extension to add image from assets

---
+++ HomeMax/SCNNode+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  SCNNode+Extensions.swift
+//  HomeMax
+//
+//  Created by Ikmal Azman on 20/12/2021.
+//
+
+import SceneKit
+
+extension SCNScene {
+    func addContentFromAssets(named name : Assets) -> SCNScene {
+        let assetsURL = "Art.scnassets/" + "\(name.rawValue)"
+        let scene = SCNScene(named: assetsURL)!
+        return scene
+    }
+}
