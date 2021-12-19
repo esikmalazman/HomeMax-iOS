e8fcf49f28866f643c02d958b246864cc95117b2
Ikmal Azman <opikmalazman@gmail.com>
2021-12-19 23:54:31 +0800

    add ar assets and material

--- HomeMax/ViewController.swift
+++ HomeMax/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import SceneKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
