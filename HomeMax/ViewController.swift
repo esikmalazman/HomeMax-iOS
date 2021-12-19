ff6b4d9ea8a2dc14131da8f1fc2ff0b65bd1dbe3
Ikmal Azman <opikmalazman@gmail.com>
2021-12-19 19:02:44 +0800

    Initial Commit

---
+++ HomeMax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeMax
+//
+//  Created by Ikmal Azman on 19/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
