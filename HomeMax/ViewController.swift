e37d92b8c8c6095729163dd06a242d4c9ae4aaa2
Ikmal Azman <opikmalazman@gmail.com>
2021-12-20 00:18:06 +0800

    add arscenview to storyboard, setup initial config for scene, and add enum to access the asset safely

--- HomeMax/ViewController.swift
+++ HomeMax/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 import SceneKit
+import ARKit
+
+enum Assets : String {
+    case bastone = "Bastone.scn"
+    case bookcase = "Bookcase.scn"
+    case dresser = "Dresser.scn"
+    case sofa = "Sofa.scn"
+}
 
 final class ViewController: UIViewController {
-
+    //MARK: - Outlets
+    @IBOutlet weak var sceneView: ARSCNView!
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scene = SCNScene().addContentFromAssets(named: .sofa)
+        sceneView.scene = scene
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    
 }
 
