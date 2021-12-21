ca1030a46a13f73b60af2a490b29692e175f1d93
Ikmal Azman <opikmalazman@gmail.com>
2021-12-21 17:45:49 +0800

    configure method to get the hit test results and place the object in 3D space

--- HomeMax/ViewController.swift
+++ HomeMax/ViewController.swift
@@ -10,10 +10,10 @@ import SceneKit
 import ARKit
 
 enum Assets : String {
-    case bastone = "Bastone.scn"
-    case bookcase = "Bookcase.scn"
-    case dresser = "Dresser.scn"
-    case sofa = "Sofa.scn"
+    case bastone = "Bastone"
+    case bookcase = "Bookcase"
+    case dresser = "Dresser"
+    case sofa = "Sofa"
 }
 
 final class ViewController: UIViewController {
@@ -23,22 +23,21 @@ final class ViewController: UIViewController {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Set view's delegate
         sceneView.delegate = self
         sceneView.debugOptions = .showFeaturePoints
-        // Create new scene
-        let scene = SCNScene().addContentFromAssets(named: .sofa)
-        // Set scene to view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let configuration = ARWorldTrackingConfiguration()
         // Enable horizontal plane detection
         configuration.planeDetection = .horizontal
+        configuration.isAutoFocusEnabled = true
         // Run view session
         sceneView.session.run(configuration)
-        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -47,25 +46,53 @@ final class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        guard let firstTouch = touches.first else {return}
-//        print("Touch : \(firstTouch.altitudeAngle)")
-//    }
+    /*
+     Hit Test Concepts
+     1. App run, user tap on screen
+     2. User tap being convert to its location
+     3. if touchesBegan method being trigger, it will find touch location as 2D spaces (x,y)
+     4. Convert the location tap to 3D coordinate in sceneView
+     5. Object will be places on detected plane
+     */
     
-    
+    // Detect touches on screen, and place the object in real world
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Detect if get the first touch
+        guard let firstTouch = touches.first else {return}
+        // Store variable for location(2D location) of detected touch in sceneView
+        let touchLocation = firstTouch.location(in: sceneView)
+        // Convert touch location (2D) to 3D coordinate, search the position in Real World
+        let results = sceneView.hitTest(touchLocation, types : .existingPlaneUsingExtent)
+        // Get the first hit test results
+        guard let hitTestResults = results.first else {return}
+        //        print("Hit Test Results \(hitTestResults)")
+        // Create and place scene at the hit test results
+        let objectScene = SCNScene().addContentFromAssets(named: .bastone)
+        let objectNode = objectScene.rootNode.childNode(withName: Assets.bastone.rawValue, recursively: true)!
+        // World Transform,  allow to get value for position and orientation in world coordinate system
+        objectNode.position = SCNVector3(
+            x:hitTestResults.worldTransform.columns.3.x,
+            y: hitTestResults.worldTransform.columns.3.y - 0.1,
+            z: hitTestResults.worldTransform.columns.3.z - 0.5
+        )
+        sceneView.scene.rootNode.addChildNode(objectNode)
+    }
 }
 //MARK: - ARSceneViewDelegate
 extension ViewController : ARSCNViewDelegate {
     // Allow to get info about width and height from ARAnchor for detected surface
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // Deterimine if detected anchor is ARPlaneAnchor
+        
+        // Determine if detected anchor is ARPlaneAnchor
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
         // Convert dimension of anchor to SCNPlane, that allow to create plane in SceneKit
         let plane = SCNPlane(width:CGFloat( planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
         // Add the material to the plane
         let planeMaterial = SCNMaterial()
         planeMaterial.diffuse.contents = UIImage(named: "grid")
         plane.materials = [planeMaterial]
+        
         // Create node for plane
         let planeNode = SCNNode()
         // Add position for planeAnchor
@@ -75,7 +102,7 @@ extension ViewController : ARSCNViewDelegate {
         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
         // Add plane to node
         planeNode.geometry = plane
-       // Add plane node to root node
+        // Add plane node to root node
         node.addChildNode(planeNode)
     }
 }
