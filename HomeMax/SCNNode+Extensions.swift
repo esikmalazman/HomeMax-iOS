66f456b6f67de998886b3b4310be7f1c7f1930c3
Ikmal Azman <opikmalazman@gmail.com>
2021-12-21 17:45:02 +0800

    refactor url path

--- HomeMax/SCNNode+Extensions.swift
+++ HomeMax/SCNNode+Extensions.swift
@@ -9,7 +9,7 @@ import SceneKit
 
 extension SCNScene {
     func addContentFromAssets(named name : Assets) -> SCNScene {
-        let assetsURL = "Art.scnassets/" + "\(name.rawValue)"
+        let assetsURL = "Art.scnassets/" + "\(name.rawValue)"+".scn"
         let scene = SCNScene(named: assetsURL)!
         return scene
     }
