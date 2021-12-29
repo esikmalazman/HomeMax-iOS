024535a016cfe412bdfd94f3d59fe9649efcbfd6
Ikmal Azman <opikmalazman@gmail.com>
2021-12-29 10:05:30 +0800

    add extensions for SceneKit to initialise 3d model in type safe

--- HomeMax/Extensions/SCNNode+Extensions.swift
+++ HomeMax/Extensions/SCNNode+Extensions.swift
@@ -8,6 +8,7 @@
 import SceneKit
 
 extension SCNScene {
+    /// Add AR models from SCNAssets from specified assets name
     func addContentFromAssets(named name : Assets) -> SCNScene {
         let assetsURL = "Art.scnassets/" + "\(name.rawValue)"+".scn"
         let scene = SCNScene(named: assetsURL)!
