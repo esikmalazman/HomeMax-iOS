0d818a6b3acb2d83c16f50ccd1978782dcf5bac9
Ikmal Azman <opikmalazman@gmail.com>
2021-12-29 16:01:03 +0800

    add assets data model

---
+++ HomeMax/Model/Assets.swift
@@ -0,0 +1,15 @@
+//
+//  AssetsType.swift
+//  HomeMax
+//
+//  Created by Ikmal Azman on 29/12/2021.
+//
+
+import Foundation
+
+enum Assets : String {
+    case bastone = "Bastone"
+    case bookcase = "Bookcase"
+    case dresser = "Dresser"
+    case sofa = "Sofa"
+}
