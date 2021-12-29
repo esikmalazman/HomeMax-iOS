7e334e311febea83f0ecc157800b4a41e67289fb
Ikmal Azman <opikmalazman@gmail.com>
2021-12-29 10:06:55 +0800

    add uiviewcontroller extensions to display toaster message and helper to add and remove child view controller

---
+++ HomeMax/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,55 @@
+//
+//  UIViewController+Extensions.swift
+//  HomeMax
+//
+//  Created by Ikmal Azman on 27/12/2021.
+//
+
+import UIKit
+
+extension UIViewController {
+    // Show toaster message to current view
+    func showToaster(withMessage message : String) {
+        let toastLabel = UILabel()
+        toastLabel.translatesAutoresizingMaskIntoConstraints = false
+        toastLabel.backgroundColor = .primaryDarkGreen
+        toastLabel.textColor = .white
+        toastLabel.font = .systemFont(ofSize: 15)
+        toastLabel.text = message
+        toastLabel.textAlignment = .center
+        toastLabel.layer.cornerRadius = 15
+        toastLabel.clipsToBounds = true
+        toastLabel.numberOfLines = 0
+        
+        self.view.addSubview(toastLabel)
+        
+        NSLayoutConstraint.activate([
+            toastLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20),
+            toastLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            toastLabel.heightAnchor.constraint(equalToConstant: 100),
+            toastLabel.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        // Animate toaster
+        UIView.animate(withDuration: 6.0, delay: 0.1, options: .curveEaseOut) {
+            toastLabel.alpha = 0.0
+        } completion: { isComplete in
+            toastLabel.removeFromSuperview()
+        }
+    }
+    
+    /// Add Child view controller to Parent view controller
+    func add(_ child : UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+    /// Remove Child view controller to Parent view controller
+    func remove(){
+        // Make user the view controller is being added to parent
+        guard parent != nil else {return}
+        willMove(toParent: self)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
