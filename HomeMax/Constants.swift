b0eac64a22e47cdfe3a09090a5260f95fb7bb21f
Ikmal Azman <opikmalazman@gmail.com>
2021-12-29 10:08:34 +0800

    add constant file to help access app theme, device height,width property and app brand color

---
+++ HomeMax/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  HomeMax
+//
+//  Created by Ikmal Azman on 27/12/2021.
+// 218380
+
+import Foundation
+import UIKit
+
+struct AppTheme {
+    static func clearDefaultNavigationBar(_ navBar : UINavigationBar) {
+        navBar.setBackgroundImage(UIImage(), for: .default)
+        navBar.shadowImage = UIImage()
+        navBar.isTranslucent = true
+    }
+}
+
+struct Device {
+    static let bounds = UIScreen.main.bounds
+    static let width = bounds.width
+    static let height = bounds.height
+}
+
+
+extension UIColor {
+    static let primaryDarkGreen = UIColor(hex: "218380")!
+    static let secondaryLightGrey = UIColor(hex: "B1B1B1")!
+}
